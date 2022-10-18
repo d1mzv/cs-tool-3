79c65b5f55b6bc02580aeaa2bd13c341fc553909
d1mzv <dmitry.maznevu@gmaile.com>
2022-10-18 02:25:19 +0200

    Added example of parsing a file to MUI table

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import logo from './smoke.webp';
+import data from './data/test.json';
+import MUIDataTable from "mui-datatables";
+
+const dataArr = [];
+var arr = data.Grenades['STEAM_1:0:573413937']
+for (var key in arr) {
+  if (arr.hasOwnProperty(key)) {
+    if (key !== "name") {
+      dataArr.push([key, arr[key].grenadeType, arr[key].name]);
+    }
+  }
+}
+console.log(dataArr);
+const columns = ["Key", "Type", "Name"];
+
+const options = {
+  filterType: "checkbox",
+};
 
 function App() {
   return (
@@ -10,12 +28,19 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Open Nade Playlist
         </a>
+        <p></p>
+        <MUIDataTable
+          title={"Dust2"}
+          data={dataArr}
+          columns={columns}
+          options={options}
+        />
       </header>
     </div>
   );
